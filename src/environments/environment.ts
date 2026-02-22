2f390a3747d07acb258d6617b25a1df2637b2de4
Moizuddin Khaja <moizuddinkhaja8040@gmail.com>
2026-02-22 15:09:58 +0530

    Removed Content of Env File

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,1 @@
-export const environment = {
-  
-    firebase: {
-    apiKey: process.env['apiKey'] || "",
-    authDomain: process.env['authDomain'] || "",
-    projectId: process.env['projectId'] || "",
-    storageBucket: process.env['storageBucket'] || "",
-    messagingSenderId: process.env['messagingSenderId'] || "",
-    appId: process.env['appId'] || ""
-  },
-  supabase: {
-    surl: process.env['surl'] || "",
-    key: process.env['key'] || ""
-  }
-};
+
